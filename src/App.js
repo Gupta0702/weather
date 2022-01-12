dee7de76fc67113f2416a00746b1d4125c427e1f
Aniket Kumar <aniketkumar049@gmail.com>
2022-01-12 08:30:58 +0000

    add promise in getco-ord call

--- src/App.js
+++ src/App.js
@@ -1,92 +1,101 @@
-import axios from 'axios'
-import React, { useState,useEffect } from 'react'
-import "./App.css"
-import Box from './Box'
-import {day,getCity} from "./util"
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Box from "./Box";
+import { day, getCity } from "./util";
 
 function App() {
   // const [currentLocation, setCurrentLocation] = useState()
-  const [data,setData]=useState({
-    city:"",
-    country:"",
-    temperature:0,
-    humidity:0,
-    windSpeed:0,
-    windDirection:"",
-    condition:{
-      type:"",
-      icon:""
-    }
-  })
-  const [forecast,setForecast]=useState([
+  const [data, setData] = useState({
+    city: "",
+    country: "",
+    temperature: 0,
+    humidity: 0,
+    windSpeed: 0,
+    windDirection: "",
+    condition: {
+      type: "",
+      icon: "",
+    },
+  });
+  const [forecast, setForecast] = useState([
     {
-      icon:"",
-      temperature:0,
-      day:"Monday"
-    }
-  ])
+      icon: "",
+      temperature: 0,
+      day: "Monday",
+    },
+  ]);
 
   useEffect(() => {
-    async function fetchdata(){
-      const city= await getCity()
-      const _url=`http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_KEY}&q=${city}&days=3&aqi=no&alerts=no`
-      const response= await axios.get(_url)
-      const data=response.data
-      const {temp_c,humidity,wind_mph,wind_dir,condition}=data.current
-      const {name,country}=data.location
-      const _forecast=data.forecast.forecastday;
+    async function fetchdata() {
+      const city = await getCity();
+      const _url = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_KEY}&q=${city}&days=3&aqi=no&alerts=no`;
+      const response = await axios.get(_url);
+      const data = response.data;
+      const { temp_c, humidity, wind_mph, wind_dir, condition } = data.current;
+      const { name, country } = data.location;
+      const _forecast = data.forecast.forecastday;
       setData({
-        city:name,
-        country:country,
-        temperature:temp_c,
-        humidity:humidity,
-        windSpeed:wind_mph,
-        windDirection:wind_dir,
-        condition:{
-          type:condition.text,
-          icon:condition.icon
-        }
-      })
+        city: name,
+        country: country,
+        temperature: temp_c,
+        humidity: humidity,
+        windSpeed: wind_mph,
+        windDirection: wind_dir,
+        condition: {
+          type: condition.text,
+          icon: condition.icon,
+        },
+      });
       setForecast([
         {
-          icon:"",
-          temperature:0,
-          day:"Monday"
-        }
+          icon: "",
+          temperature: 0,
+          day: "Monday",
+        },
       ]);
-      for(let i=0;i<3;i++){
-        setForecast(prevState=>[
+      for (let i = 0; i < 3; i++) {
+        setForecast((prevState) => [
           ...prevState,
           {
-            icon:_forecast[i].day.condition.icon,
-            temperature:_forecast[i].day.avgtemp_c,
-            day: day[i]
-          }
-        ])
+            icon: _forecast[i].day.condition.icon,
+            temperature: _forecast[i].day.avgtemp_c,
+            day: day[i],
+          },
+        ]);
       }
       console.log(forecast);
     }
     fetchdata();
-  }, [])
+  }, []);
 
   return (
     <div>
       <div className="search">
-        <input type="text" className='input' placeholder='Enter city name'/>
+        <input type="text" className="input" placeholder="Enter city name" />
       </div>
       <div className="container">
         <h2 id="currentTemperature">{data.temperature} °C</h2>
         <img id="icon" src={data.condition.icon} alt="" />
         <div className="today">
-          <h3 id="location">{data.city},{data.country}</h3>
+          <h3 id="location">
+            {data.city},{data.country}
+          </h3>
           <h4 id="day">{day[0]}</h4>
           <h4 id="humidity">humidity: {data.humidity}%</h4>
-          <h4 id="wind">Wind: {data.windDirection}, {data.windSpeed} mph</h4>
+          <h4 id="wind">
+            Wind: {data.windDirection}, {data.windSpeed} mph
+          </h4>
         </div>
       </div>
-      <div className='container-2'>
-        {forecast.map(data=>{
-          <Box key="" day={data.day} icon={data.icon} temperature={data.temperature} />
+      <div className="container-2">
+        {forecast.map((data) => {
+          <Box
+            key=""
+            day={data.day}
+            icon={data.icon}
+            temperature={data.temperature}
+          />;
         })}
 
         {/* <Box day={forecast[0].day} icon={forecast[0].icon} temperature={forecast[0].temperature}/>
@@ -94,10 +103,9 @@ function App() {
         <Box day={forecast[0].day} icon={forecast[0].icon} temperature={forecast[0].temperature}/>
         <Box day={forecast[0].day} icon={forecast[0].icon} temperature={forecast[0].temperature}/>
         <Box day={forecast[0].day} icon={forecast[0].icon} temperature={forecast[0].temperature}/>  */}
-
       </div>
     </div>
-  )
-};
+  );
+}
 
 export default App;
--- src/util.js
+++ src/util.js
@@ -1,40 +1,47 @@
 import axios from "axios";
 
-const days=[
+const days = [
   "Sunday",
   "Monday",
   "Tuesday",
   "Wednesday",
   "Thursday",
   "Friday",
-  "Saturday"
-]
+  "Saturday",
+];
 
-const date= new Date();
-let dayIndex=date.getDay();
+const date = new Date();
+let dayIndex = date.getDay();
 
-const day=[days[dayIndex],days[(dayIndex+1)%7],days[(dayIndex+2)%7]]
+const day = [
+  days[dayIndex],
+  days[(dayIndex + 1) % 7],
+  days[(dayIndex + 2) % 7],
+];
 
-const getCoordinates=()=>{
-  let coordinates;
-  navigator.geolocation.getCurrentPosition(position=>{
-    const {latitude,longitude}=position.coords
-    coordinates=[latitude.toString(),longitude.toString()];
-  })
-  return coordinates
-}
+const getCoords = async () => {
+  const pos = await new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
 
+  return {
+    long: pos.coords.longitude,
+    lat: pos.coords.latitude,
+  };
+};
 
-const getCity = async ()=>{
-  const coordinates=await getCoordinates()
-  const lat=coordinates[0];
-  const long=coordinates[1];
-  const url=`https://us1.locationiq.com/v1/reverse.php?key=${REACT_APP_LOCATION_KEY}&lat=${lat}&lon=${long}&format=json`;
-  const response= await axios.get(url);
-  const city= response.address.suburb
-  return city
-}
+const getCity = async () => {
+  const coordinates = await getCoords();
+  console.log(
+    "🚀 ~ file: util.js ~ line 33 ~ getCity ~ coordinates",
+    coordinates
+  );
+  const { lat, long } = coordinates;
+  const url = `https://us1.locationiq.com/v1/reverse.php?key=${process.env.REACT_APP_LOCATION_KEY}&lat=${lat}&lon=${long}&format=json`;
+  const response = await axios.get(url);
+  const city = response.address.suburb;
+  console.log("🚀 ~ file: util.js ~ line 43 ~ getCity ~ city", city);
+  return city;
+};
 
-
-export  {day,getCity};
-
+export { day, getCity };
